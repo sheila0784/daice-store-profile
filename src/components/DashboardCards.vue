bc6a52e43eb29933c678529dde8944117eef4ca8
Sheila Mae <sheilamaedandoy@gmail.com>
2026-06-07 16:06:39 +0800

    update

--- src/components/DashboardCards.vue
+++ src/components/DashboardCards.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="flex flex-col gap-6 mt-1 justify-center items-center">
-    
     <!-- CARDS (BOTTOM) -->
     <div class="flex gap-2 flex-wrap mt-4">
-      <Card class="shadow-2 border-gray-300 w-[180px] md:w-[192px] h-[130px] md:h-[144px] bg-blue-100">
+      <Card
+        class="shadow-2 border-gray-300 w-[180px] md:w-[192px] h-[130px] md:h-[144px] bg-blue-100"
+      >
         <template #title>
           <span class="flex text-xl font-extrabold justify-center"> Total Dealers </span>
         </template>
@@ -13,7 +14,9 @@
         </template>
       </Card>
 
-      <Card class="shadow-2 border-gray-300 w-[180px] md:w-[192px] h-[130px] md:h-[144px] bg-green-100">
+      <Card
+        class="shadow-2 border-gray-300 w-[180px] md:w-[192px] h-[130px] md:h-[144px] bg-green-100"
+      >
         <template #title>
           <span class="flex text-xl font-extrabold justify-center"> Total Customers </span>
         </template>
@@ -23,9 +26,9 @@
         </template>
       </Card>
 
-     
-
-      <Card class="shadow-2 border-gray-300 w-[180px] md:w-[192px] h-[130px] md:h-[144px] bg-purple-100">
+      <Card
+        class="shadow-2 border-gray-300 w-[180px] md:w-[192px] h-[130px] md:h-[144px] bg-purple-100"
+      >
         <template #title>
           <span class="flex text-xl font-extrabold justify-center"> Total Riders </span>
         </template>
@@ -54,32 +57,50 @@
       </div>
     </div>
 
-    <div class="flex items-center flex-wrap justify-center w-full p-6 text-sm">
-      <Datatable :value="salesData" class="w-full">
-        <Column field="order_date" header="Date" :body="(data) => new Date(data.date).toLocaleDateString()"></Column>
-        <Column field="dealer" header="Dealer"></Column>
-        <Column field="no_of_served_customers" header="Served Customers"></Column>
-        <Column field="total_amount" header="Total Sales"></Column>
+    <div class="flex items-center flex-wrap justify-center w-full text-sm">
+      <Datatable :value="salesData" class="w-full border-1 border-gray-300" :rows="rows" :rowsPerPageOptions="rowsPerPageOptions" paginator>
+        <Column
+          field="order_date"
+          header="Date"
+          :body="(data) => new Date(data.date).toLocaleDateString()"
+          sortable
+          headerClass="bg-yellow-50"
+        ></Column>
+        <Column field="dealer" header="Dealer" headerClass="bg-yellow-50"></Column>
+        <Column field="no_of_served_customers" header="Served Customers" headerClass="bg-yellow-50"></Column>
+        <Column field="total_amount" headerClass="text-right bg-yellow-50" header="Total Sales"  bodyClass="text-right" sortable>
+          <template #body="{ data }">
+            {{ formatNumber(data.total_amount) }}
+          </template>
+        </Column>
       </Datatable>
-     </div> 
-
+    </div>
   </div>
 </template>
 
 <script setup>
 import Card from "primevue/card";
 import DatePicker from "primevue/datepicker";
-import { ref, watch , onMounted} from "vue";
+import { ref, watch, onMounted } from "vue";
 import { formatDateLabel } from "@/utils/date";
 import { useDashboardCards } from "@/composables/useDashboardCards";
 import Datatable from "primevue/datatable";
 import Column from "primevue/column";
 
-
 const dateRange = ref(null);
 const dateDisplay = ref("");
 
-const { salesData, fetchDashboardCards, fetchCounts, dealerCount, customerCount, riderCount } = useDashboardCards(dateRange);
+const { salesData, fetchDashboardCards, fetchCounts, dealerCount, customerCount, riderCount, rows, rowsPerPageOptions } =
+  useDashboardCards(dateRange);
+
+const formatNumber = (value) => {
+  if (value == null) return "0";
+
+  return new Intl.NumberFormat("en-US", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(value);
+};
 
 watch(dateRange, (newVal) => {
   dateRange.value = newVal;
@@ -89,12 +110,10 @@ watch(dateRange, (newVal) => {
   fetchDashboardCards();
   fetchCounts();
   console.log("sales data:", salesData.value);
-
 });
 
 onMounted(() => {
   fetchDashboardCards();
   fetchCounts();
 });
-
 </script>
--- src/components/StoresList.vue
+++ src/components/StoresList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen">
+  <div class="min-h-screen text-sm">
     <MenuBar />
     <Toast />
     <ConfirmDialog />
